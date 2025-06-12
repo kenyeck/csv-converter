25e9ebb513b5c09b62c33e036a6c7bc59f292c06
Ken Yeck <ken.yeck@gmail.com>
2025-06-11 17:30:40 -0700

    Suppress hydration warning

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,8 +2,9 @@ import { Nav } from '@components/Nav';
 import { Provider } from './Provider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  // Note hydration warning is suppressed due to ThemeToggle component
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <Provider><Nav>{children}</Nav></Provider>
       </body>
