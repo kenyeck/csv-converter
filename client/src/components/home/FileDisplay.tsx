54272a3ca1503664201d90be17a791ea2c015384
Ken Yeck <ken.yeck@gmail.com>
2025-06-13 12:58:08 -0700

    Added FileDisplay component to Home

---
+++ client/src/components/home/FileDisplay.tsx
@@ -0,0 +1,5 @@
+import { Box, BoxProps } from '@chakra-ui/react';
+
+export const FileDisplay: React.FC<BoxProps> = (props) => {
+   return <Box {...props}>FILE DISPLAY</Box>;
+};
--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,19 +1,30 @@
 'use client';
 
 import { Stack, Box, Separator } from '@chakra-ui/react';
-import { FileUploader } from '@components/home/FileUploader';
+import { FileUpload } from '@components/home/FileUpload';
+import { useState } from 'react';
+import { FileDisplay } from './FileDisplay';
 
 export const Home = () => {
+   const [uploadFile, setUploadFile] = useState<File | null>(null);
+
+   const onUpload = (file: File) => {
+      console.log('File to upload:', file.name);
+      setUploadFile(file);
+   };
+
    return (
       <Stack p={8} direction={'column'} alignItems={'center'}>
          <Stack mb={20} direction={'column'} alignItems={'center'} maxW={'65%'} gap={4}>
-            <Box fontSize={'3em'} fontWeight={'bold'} lineHeight={'1em'} textAlign={'center'}>Convert CSV & Excel to Clean, Structured Data — Instantly</Box>
-            <Box fontSize={'1.25em'}  textAlign={'center'}>
+            <Box fontSize={'3em'} fontWeight={'bold'} lineHeight={'1em'} textAlign={'center'}>
+               Convert CSV & Excel to Clean, Structured Data — Instantly
+            </Box>
+            <Box fontSize={'1.25em'} textAlign={'center'}>
                Drop your files in. Get perfect JSON, XML, or CSV in seconds. No code, no setup —
                just fast, browser-based data conversion.
             </Box>
          </Stack>
-         <FileUploader height={'100vh'} />
+         {uploadFile ? <FileDisplay /> : <FileUpload onUpload={onUpload} />}
          <Separator colorPalette={'black'} />
          <Box height={'300px'} w={'100%'} textAlign={'center'} pt={'75px'}>
             MARKETING SECTION
