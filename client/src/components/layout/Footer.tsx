556458532efa91c15a824344580ab3eee3a2b0f8
Ken Yeck <ken.yeck@gmail.com>
2025-06-13 11:38:18 -0700

    Footer update

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -3,18 +3,20 @@ import { Box, Link, Stack, Separator } from '@chakra-ui/react';
 export const Footer = () => {
    return (
       <>
-         <Separator />
-         <footer style={{ textAlign: 'center', padding: '20px', marginTop: 'auto' }}>
-            <Stack direction="row" justifyContent="center" gap={10} mb={5}>
-               <Link href="/" fontWeight={'600'} textDecoration={'none'}>
-                  Home
-               </Link>
-            </Stack>
-            <Box fontSize={'1.25em'} fontWeight={'bold'}>
-               CSVConvert
-            </Box>
-            <Box fontSize={'.85em'} color={'fg.muted'} mt={5}>
-               © {new Date().getFullYear()} CSVConvert. All rights reserved.
+         <footer style={{ textAlign: 'center', marginTop: 'auto' }}>
+            <Separator />
+            <Box padding={'20px'}>
+               <Stack direction="row" justifyContent="center" gap={10} mb={5}>
+                  <Link href="/" fontWeight={'600'} textDecoration={'none'}>
+                     Home
+                  </Link>
+               </Stack>
+               <Box fontSize={'1.25em'} fontWeight={'bold'}>
+                  CSVConvert
+               </Box>
+               <Box fontSize={'.85em'} color={'fg.muted'} mt={5}>
+                  © {new Date().getFullYear()} CSVConvert. All rights reserved.
+               </Box>
             </Box>
          </footer>
       </>
